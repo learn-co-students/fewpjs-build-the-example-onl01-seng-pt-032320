d9184097193a9ae9916e713363cb0b8e9806cdaa
github username <github email address>
2020-10-02 15:39:54 -0400

    Done.

--- main.js
+++ main.js
@@ -1,9 +1,39 @@
 // Defining text characters for the empty and full hearts for you to use later.
-const EMPTY_HEART = '♡'
-const FULL_HEART = '♥'
+const EMPTY_HEART = '♡';
+const FULL_HEART = '♥';
+const errorModal = document.getElementById("modal");
+errorModal.classList.add("hidden");
+const modalH2 = document.querySelector("#modal h2");
+const url = "http://mimicServer.example.com";
+const likeGlyph = document.querySelectorAll(".like-glyph");
 
-// Your JavaScript code goes here!
-
+document.addEventListener("DOMContentLoaded", () => {
+  
+  function glyphListener() {
+  likeGlyph.forEach(glyph => glyph.addEventListener("click", (e) => {
+    if (glyph.innerHTML === EMPTY_HEART) {
+      glyph.innerHTML = FULL_HEART;
+    } else if (glyph.innerHTML === FULL_HEART) {
+      glyph.innerHTML = EMPTY_HEART;
+    }
+    glyph.classList.toggle("activated-heart")
+   
+  }));
+  };
+    mimicServerCall("http://mimicServer.example.com")
+      .then(function(object) {
+        console.log(object);
+        glyphListener();
+      })
+      .catch(function(error) {
+        modal.classList.remove("hidden");
+        modalH2.innerHTML = error.message;
+        setTimeout(function() {
+          modal.classList.add("hidden")
+        }, 5000);
+      });
+  
+});
 
 
 
