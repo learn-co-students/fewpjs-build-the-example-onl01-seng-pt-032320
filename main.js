e561304877f58666a9689ab92c3ef8576a5c26b8
Allan5829 <allan.farca@gmail.com>
2020-10-07 15:38:14 -0700

    Done.

--- main.js
+++ main.js
@@ -3,8 +3,41 @@ const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
+document.getElementById("modal").classList.add("hidden");
+const likeButtons = document.querySelectorAll(".like");
 
+likeButtons.forEach(function(button) {
+  button.addEventListener("click", function(e) {
+    processLike(e);
+  })
+});
 
+function processLike(object) {
+  if (object.target.classList.value.includes("activated-heart")) {
+    console.log("yes heart");
+    object.target.classList.remove("activated-heart");
+    const heart = object.target.querySelector("span");
+    heart.innerText = EMPTY_HEART;
+  } else {
+    console.log("no heart");
+    mimicServerCall()
+    .then(function() {likeHandle(object.target)})
+    .catch(function(obj) {errorHandle(obj)});
+  }
+}
+
+function likeHandle(obj) {
+  obj.classList.add("activated-heart");
+  const heart = obj.querySelector("span");
+  heart.innerText = FULL_HEART;
+}
+
+function errorHandle(error) {
+  const errorText = document.getElementById("modal-message")
+  errorText.innerText = error.message
+  document.getElementById("modal").classList.remove("hidden");
+  setTimeout(function(){ document.getElementById("modal").classList.add("hidden"); }, 5000);
+}
 
 
 //------------------------------------------------------------------------------
